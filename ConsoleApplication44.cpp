90fdde8955b8279f66ba8c4e1cb889a9c5c928dc
GetChara() <133586791+SwitchGetch@users.noreply.github.com>
2023-12-17 18:31:05 +0300

    Add files via upload

---
+++ ConsoleApplication44.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include <filesystem>
+#include <fstream>
+using namespace std;
+using namespace filesystem;
+
+void allFolders(path p)
+{
+    for (auto& element : directory_iterator(p))
+    {
+        if (is_directory(element))
+        {
+            cout << element.path() << endl;
+
+            allFolders(element);
+        }
+    }
+}
+
+int main()
+{
+    path p = "C:/Program Files (x86)";
+
+    allFolders(p);
+}
